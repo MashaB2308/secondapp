c712e65aac15eb92ae8b2343d3581b587ad37058
MashaB2308 <MashaB2308@yandex.ru>
2025-02-03 19:29:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+alert("Hello world")
+//alert("26.01.2025")
+console.log(8)
+var age = 24
+console.log(age)
+let firstName = "Mary"
+firstName = "Kate"
+console.log(firstName)
+const lastName = "Belova"
+console.log(lastName)
+console.log(2>4)
+let nuller = null
+console.log(nuller)
+
+
+console.log(typeof 10) //number
+console.log(typeof "а") //string
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+console.log(typeof {}) //object
+console.log(typeof true) //boolean
+console.log(typeof false) //boolean
+
+let name = "Маша"
+console.log("Добро пожаловать на курс " + name)
+
+document.getElementById
